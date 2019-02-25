921331b9bdb261b6cdee9732f65ca5e7ae05b149
Artashes <you@example.com>
2019-02-25 23:14:10 +0400

    task remove added, some other changes

--- to_do_list.js
+++ to_do_list.js
@@ -10,12 +10,18 @@ class ToDoList {
 
   createGroup() {
     console.log('TEST this', this);
-    let ul = document.createElement('ul');
-    let group_id = 'Group_' + this.group_index;
-    ul.id = group_id;
-    this.group_index++;
-    ul.className = 'Group';
+
+    let div_for_group = document.createElement('div');
+    let div_for_group_id = 'div_for_group_' + this.group_index;
+    div_for_group.id = div_for_group_id;
+    div_for_group.className = 'Div_For_Group';
+
+    let ul_group = document.createElement('ul');
     let group_name = "Group...";
+    let ul_group_id = 'ul_group_' + this.group_index;
+    ul_group.id = ul_group_id;
+    this.group_index++;
+    ul_group.className = 'ul_Group';
 
     let li = document.createElement("li");
 
@@ -23,7 +29,7 @@ class ToDoList {
     group_input_field.placeholder = group_name;
 
     li.appendChild(group_input_field);
-    ul.appendChild(li);
+    ul_group.appendChild(li);
 
     li = document.createElement("li");
     let task_initializer = document.createElement("input");
@@ -43,11 +49,13 @@ class ToDoList {
 
     li.appendChild(task_initializer);
     li.appendChild(add_task_button);
-    ul.appendChild(li);
-    document.getElementById("div_1").appendChild(ul);
+    ul_group.appendChild(li);
+    div_for_group.appendChild(ul_group);
+    document.getElementById("main_div").appendChild(div_for_group);
 
-    function createTask(group_id, task_initializer_id, obj) { 
-      console.log(group_id, task_initializer_id);
+    function createTask(ul_group_id, task_initializer_id, obj) {
+      console.log('ul_group_id, task_initializer_id:',
+                  ul_group_id, task_initializer_id);
       let li = document.createElement("li");
       li.id = 'task_li_' + obj.task_index;
       li.draggable="true";
@@ -66,32 +74,50 @@ class ToDoList {
       }
 
       li.appendChild(task);
-      document.getElementById(group_id).appendChild(li);
+      document.getElementById(ul_group_id).appendChild(li);
       document.getElementById(task_initializer_id).value = "";
 
-      // let span = document.createElement("SPAN");
-      // let txt = document.createTextNode("\u00D7");
-      // span.className = "close";
-      // span.appendChild(txt);
-      // li.appendChild(span);
+      let span_remove_task = document.createElement("SPAN");
+      let text_remove_task = document.createTextNode("Remove");
+      span_remove_task.className = "remove_task";
+      span_remove_task.appendChild(text_remove_task);
+      li.appendChild(span_remove_task);
+
+      span_remove_task.onclick = function() {
+        // in this context 'this' is span_remove_task
+
+        // does not remove, but hides
+        // let parent = this.parentElement;
+        // parent.style.display = "none";
+
+        // ---- two code snippets below do the same thing (parentElement/parentNode) ---
+
+        let parent = this.parentElement;
+        parent.parentElement.removeChild(parent);
+
+        // let parent = this.parentNode;
+        // parent.parentNode.removeChild(parent);
+
+        // ----------------------------------------------------------------------------
+      }
 
       obj.task_index++;
     }
 
-    function enterKeyPressAddTask(event, group_id, task_initializer_id, obj) {
-        const x = event.which || event.keyCode;
-        if (x == 13) {
-          createTask(group_id, task_initializer_id, obj);
+    function enterKeyPressAddTask(event, ul_group_id, task_initializer_id, obj) {
+      const x = event.which || event.keyCode;
+      if (x == 13) {
+        createTask(ul_group_id, task_initializer_id, obj);
     }
   }
     console.log('TEST this', this);
 
     let obj = this;
     add_task_button.onclick = function() {
-      createTask(group_id, task_initializer_id, obj)
+      createTask(ul_group_id, task_initializer_id, obj)
     };
     task_initializer.onkeypress = function() {
-      enterKeyPressAddTask(event, group_id, task_initializer_id, obj)
+      enterKeyPressAddTask(event, ul_group_id, task_initializer_id, obj)
     };
   }
 }
